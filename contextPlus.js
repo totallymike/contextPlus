9916c870776494f3d88744a0d652199ebf933570
Michael Westbom <michael.westbom@maxwellhealth.com>
2017-03-09 18:55:30 -0500

    Don't trigger the addon if it can't do its job

--- contextPlus.js
+++ contextPlus.js
@@ -1,45 +1,47 @@
-browser.contextualIdentities.query({})
-  .then((contexts) => {
-    const parentId = chrome.contextMenus.create({
-      id: "moveContext",
-      title: "Move to context",
-      contexts: ["tab"]
-    });
-
-    const contextStore = contexts.reduce((store, context) => {
-      return Object.assign({}, store, {
-        [`contextPlus-${context.name}`]: context.cookieStoreId
+if (browser.contextualIdentities !== undefined) {
+  browser.contextualIdentities.query({})
+    .then((contexts) => {
+      const parentId = chrome.contextMenus.create({
+        id: "moveContext",
+        title: "Move to context",
+        contexts: ["tab"]
       });
-    }, {});
 
-    contexts.forEach(context => {
-      chrome.contextMenus.create({
-        type: "normal",
-        title: context.name,
-        id: `contextPlus-${context.name}`,
-        parentId
+      const contextStore = contexts.reduce((store, context) => {
+        return Object.assign({}, store, {
+          [`contextPlus-${context.name}`]: context.cookieStoreId
+        });
+      }, {});
+
+      contexts.forEach(context => {
+        chrome.contextMenus.create({
+          type: "normal",
+          title: context.name,
+          id: `contextPlus-${context.name}`,
+          parentId
+        });
+      });
+
+      browser.contextMenus.onClicked.addListener(function (info, tab) {
+        if (contextStore.hasOwnProperty(info.menuItemId)) {
+          const cookieStoreId = contextStore[info.menuItemId];
+          const newTabData = {
+            active,
+            index,
+            pinned,
+            url,
+            windowId
+          } = tab;
+
+          browser.tabs.create({
+            active,
+            cookieStoreId,
+            index,
+            pinned,
+            url,
+            windowId
+          }).then(() => browser.tabs.remove(tab.id));
+        }
       });
     });
-
-    browser.contextMenus.onClicked.addListener(function (info, tab) {
-      if (contextStore.hasOwnProperty(info.menuItemId)) {
-        const cookieStoreId = contextStore[info.menuItemId];
-        const newTabData = {
-          active,
-          index,
-          pinned,
-          url,
-          windowId
-        } = tab;
-
-        browser.tabs.create({
-          active,
-          cookieStoreId,
-          index,
-          pinned,
-          url,
-          windowId
-        }).then(() => browser.tabs.remove(tab.id));
-      }
-    });
-  });
+}
