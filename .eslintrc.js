429a6ddf49e24ff2f650ca5aa29927170868db30
Jonathan Kingston <jonathan@jooped.co.uk>
2017-06-06 12:14:39 +0100

    Cleanup the code to use ESNext latest and adding linting.

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "parserOptions": {
+        "ecmaVersion": 8
+    },
+    "globals": {
+      "browser": true
+    },
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
